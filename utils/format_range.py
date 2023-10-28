03c0ece77e66567e6be841adbd4ff8fc629f8757
karamelniycoder <109586521+karamelniycoder@users.noreply.github.com>
2023-10-28 16:30:54 +0700

    fix sleep bug

--- utils/format_range.py
+++ utils/format_range.py
@@ -4,7 +4,8 @@ from random import randint
 
 def format_range(value: str | int,
                  return_randint: bool = False) -> tuple[int, int] | int:
-    if value.isdigit():
+    if type(value) == int: return value 
+    elif str(value).isdigit():
         return randint(a=int(value),
                        b=int(value)) if return_randint else int(value), int(value)
 
